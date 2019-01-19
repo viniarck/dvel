4aabfcb08a48471a3d70968cac32388ec5553577
Vinicius Arcanjo <viniarck@gmail.com>
2019-01-19 19:55:22 -0200

    Updated requirements.txt and settings.py

--- settings.py
+++ settings.py
@@ -7,3 +7,26 @@ dpids = [
     '00:00:00:00:00:00:00:01', '00:00:00:00:00:00:00:02',
     '00:00:00:00:00:00:00:03', '00:00:00:00:00:00:00:04'
 ]
+
+# kytos http server
+http_server = "localhost"
+# kytos http port
+http_port = "8181"
+# dvel endpoint
+endpoint = "api/viniarck/dvel/changelane"
+# influx db server
+db_server = "localhost"
+# influx db name
+db_name = "dvel"
+
+# frequency to eval the async loop
+frequency = 0.5
+# timeout to detect loss when sending requests to the db
+timeout = 3
+params = {"l_rtt_key": "d3", "max_rtt": 1.0e4}
+# containers names and their respective lanes
+containers = {
+    "d3": {"rtt": 0.0, "pkt_loss": 0, "evc_path": 1},
+    "d4": {"rtt": 0, "pkt_loss": 0.0, "evc_path": 2},
+    "d5": {"rtt": 0.0, "pkt_loss": 0, "evc_path": 3},
+}
